e627c002b16fa2583124ec9d8a46d2b0a87deaa4
Niels Louwes <niels.louwes@and.digital>
2022-01-31 22:15:33 +0100

    added some submit and change functions

--- src/App.js
+++ src/App.js
@@ -15,27 +15,15 @@ function App({tasks}) {
     />
   ));
 
+  function addTask(name) {
+    alert(name);
+  }
+
 
   return (
     <div className="todoapp stack-large">
       <h1>To Do</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         <button type="button" className="btn toggle-btn" aria-pressed="true">
           <span className="visually-hidden">Show </span>
